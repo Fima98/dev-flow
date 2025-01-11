6ff7add69f6dfb8aa1be58e6de8a9edfc67c2b65
Fima <loboda.fima@icloud.com>
2025-01-11 22:27:10 +0200

    Assets and Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,26 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const inter = localFont({
+  src: "./fonts/InterVF.ttf",
+  variable: "--font-inter",
+  weight: "100 200 300 400 500 700 800 900",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const spaceGrotesk = localFont({
+  src: "./fonts/SpaceGroteskVF.ttf",
+  variable: "--font-space-grotesk",
+  weight: "300 400 500 700 800 900",
 });
 
 export const metadata: Metadata = {
-  title: "Dev Flow",
-  description: "Better version of Stack Overflow",
+  title: "DevFlow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help. share knowledge and collaborate with other developers. Join the DevFlow community today!",
+  icons: {
+    icon: "images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -25,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 const Home = () => {
-  return <h1 className="h1-bold  text-primary-500">Hello</h1>;
+  return (
+    <>
+      <h1 className="h1-bold text-primary-500">
+        Hello world i hope this is working
+      </h1>
+      <h1 className="h1-bold font-space-grotesk text-primary-500">
+        Hello world i hope this is working
+      </h1>
+    </>
+  );
 };
 
 export default Home;
