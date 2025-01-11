4bbe098ed75e9d9d9661b3f924193567bce4bdce
Fima <loboda.fima@icloud.com>
2025-01-11 18:32:28 +0200

    Tailwind CSS typography

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,7 +16,6 @@ export default [
   ...compat.extends(
     "next/core-web-vitals",
     "next/typescript",
-    "standard",
     "plugin:tailwindcss/recommended",
     "prettier"
   ),
@@ -26,6 +26,32 @@ export default [
       "import/no-anonymous-default-export": ["off"],
       "@typescript-eslint/no-require-imports": ["off"],
       "tailwindcss/no-custom-classname": ["off"],
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin", // Built-in types are first
+            "external", // External libraries
+            "internal", // Internal modules
+            ["parent", "sibling"], // Parent and sibling types can be mingled together
+            "index", // Then the index file
+            "object", // Object imports
+          ],
+          "newlines-between": "always",
+          pathGroups: [
+            {
+              pattern: "@app/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
     },
   },
 ];
