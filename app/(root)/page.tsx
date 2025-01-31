9580faef47c74b09c34e9ff7908262a9b45d28cd
Fima <loboda.fima@icloud.com>
2025-01-31 18:17:13 +0200

    Add authentication middleware, routes, and social sign-in functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,8 @@
-const Home = () => {
+import { signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
   return (
     <>
       <h1 className="h1-bold text-primary-500">
@@ -7,6 +11,15 @@ const Home = () => {
       <h1 className="h1-bold font-space-grotesk text-primary-500">
         Hello world i hope this is working
       </h1>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_UP });
+        }}
+      >
+        <Button type="submit">Log out</Button>
+      </form>
     </>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
-import "./globals.css";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import { ThemeProvider } from "next-themes";
+import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
+
+import "./globals.css";
+import { auth } from "@/auth";
+import { Toaster } from "@/components/ui/toaster";
+import ThemeProvider from "@/context/Theme";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -12,37 +17,40 @@ const inter = localFont({
 const spaceGrotesk = localFont({
   src: "./fonts/SpaceGroteskVF.ttf",
   variable: "--font-space-grotesk",
-  weight: "300 400 500 700 800 900",
+  weight: "300 400 500 700",
 });
 
 export const metadata: Metadata = {
   title: "DevFlow",
   description:
-    "A community-driven platform for asking and answering programming questions. Get help. share knowledge and collaborate with other developers. Join the DevFlow community today!",
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
   icons: {
-    icon: "images/site-logo.svg",
+    icon: "/images/site-logo.svg",
   },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+  const session = await auth();
+
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+      <SessionProvider session={session}>
+        <body
+          className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
         >
-          {children}
-        </ThemeProvider>
-      </body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+          <Toaster />
+        </body>
+      </SessionProvider>
     </html>
   );
-}
+};
+
+export default RootLayout;
